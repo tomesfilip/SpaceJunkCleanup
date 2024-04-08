c301adcfa9df1f9e1fe8c5b43fb4963bb0e1c29d
Filip Tomes <filiptomes@protonmail.com>
2024-04-08 10:59:27 +0200

    no movement beyond game plan

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,6 +19,8 @@ func _physics_process(delta):
 	velocity = direction * speed
 	move_and_slide()
 	
+	global_position = global_position.clamp(Vector2.ZERO, get_viewport_rect().size)
+	
 func shoot():
 	laser_shot.emit(laser_scene, muzzle.global_position)
 	
