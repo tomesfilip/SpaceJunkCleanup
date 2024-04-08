7b282683b90d5f5173b43f86216b1056a0c1149a
Filip Tomes <filiptomes@protonmail.com>
2024-04-08 10:23:33 +0200

    space junk

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -21,7 +21,7 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 
 func _on_area_entered(area):
-	if area is Asteroid:
+	if area is Asteroid or area is SpaceJunk:
 		area.destroy()
 		queue_free()
 		
--- scripts/space_junk.gd
+++ scripts/space_junk.gd
@@ -1,6 +1,6 @@
-class_name Asteroid extends Area2D
+class_name SpaceJunk extends Area2D
 
-@export var speed = 100
+@export var speed = 150
 
 func _physics_process(delta):
 	global_position.y += speed * delta
@@ -8,12 +8,10 @@ func _physics_process(delta):
 func destroy():
 	queue_free()
 
-
 func _on_body_entered(body):
 	if body is Player:
 		body.destroy()
 		destroy()
-
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
